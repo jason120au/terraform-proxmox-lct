017553494392d1df4a91420c86c67386ac1f4836
Jason Campbell <jasonc@jasonc.au>
2026-01-21 10:22:56 +1100

    change for API auth

--- main.tf
+++ main.tf
@@ -37,7 +37,8 @@ terraform {
 provider "proxmox" {
     pm_tls_insecure = true
     pm_api_url = "https://proxmoxsiz.jason120au.net/api2/json"
-    pm_password = var.proxmox_password
+    pm_api_token_id = var.proxmox_user
+    pm_api_token_secret = var.proxmox_password
     pm_user = var.proxmox_user
     pm_otp = ""
    
